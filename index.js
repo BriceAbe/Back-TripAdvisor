ff05367ae14aaa1b888c406f7a59d2bcb6fb200c
brice <brice@MacBook-Pro-de-brice.local>
2020-11-01 23:32:55 +0100

    fourth commit

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const api_key = process.env.MAILGUN_API_KEY;
 const domain = process.env.MAILGUN_DOMAIN;
 const mailgun = require("mailgun-js")({ apiKey: api_key, domain: domain });
 
+app.get("/", (req, res) => {
+  res.status(200).json("Bienvenue sur le back");
+});
+
 app.post("/", (req, res) => {
   console.log(req.fields);
   const { firstName, lastName, email } = req.fields;
