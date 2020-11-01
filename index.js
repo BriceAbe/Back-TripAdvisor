f6420bdc45ae80f0747e1016a9e5103fb583624a
brice <brice@MacBook-Pro-de-brice.local>
2020-11-01 23:14:45 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const formidable = require("express-formidable");
+const cors = require("cors");
+
+const app = express();
+app.use(formidable());
+app.use(cors());
+
+const api_key = process.env.MAILGUN_API_KEY;
+const domain = process.env.MAILGUN_DOMAIN;
+const mailgun = require("mailgun-js")({ apiKey: api_key, domain: domain });
+
+app.post("/", (req, res) => {
+  console.log(req.fields);
+  const { firstName, lastName, email } = req.fields;
+  const data = {
+    from: `${firstName} ${lastName} <${email}>`,
+    to: "bricebrain@gmail.com, YOU@YOUR_DOMAIN_NAME",
+    subject: "Go Reacteur",
+    text: "Hello merci d'avoir tester mon formulaire!",
+  };
+  mailgun.messages().send(data, function (error, body) {
+    console.log(body);
+  });
+  res.status(200).json({ message: req.fields });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("server connecté");
+});
